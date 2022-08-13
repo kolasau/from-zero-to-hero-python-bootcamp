fb5c954c9cd2d94ee8fe2d57e98aebad42dc0adf
kolasau <maksim.kolasau@mapbox.com>
2022-08-13 13:55:50 +0300

    complited game

--- 047 Interactions between Python Functions.py
+++ 047 Interactions between Python Functions.py
@@ -1,19 +1,34 @@
 from random import shuffle
 
-example = [1, 2, 3, 4, 5, 6, 7]
-shuffle(example)
-print(example)
-
 
 def shuffle_list(mylist):
     shuffle(mylist)
     return mylist
 
 
-result = shuffle_list(example)
-print(result)
+def player_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        guess = input("Pick a number: 0, 1, or 2 -> ")
+    return int(guess)
 
-# where is 'O' ? game
-mylist = [' ', 'O', ' ']
-where_is_O = shuffle_list(mylist)
-print(where_is_O)
+
+def check_guess(mylist, guess):
+    if mylist[guess] == 'O':
+        print('Correct')
+    else:
+        print('Wrong guess!')
+        print(mylist)
+
+
+#  INITIAL LIST
+mylist = ['', 'O', '']
+
+#  SHUFFLE LIST
+mixedup_list = shuffle_list(mylist)
+
+#  USER GUESS
+guess = player_guess()
+
+#  CHECK GUESS
+check_guess(mixedup_list, guess)
