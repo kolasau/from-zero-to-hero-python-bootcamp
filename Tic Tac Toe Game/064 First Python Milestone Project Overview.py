f2ff3eb12b6b692115ef8e303ab9bf333593fb2f
kolasau <maksim.kolasau@mapbox.com>
2022-08-26 17:01:03 +0300

    latest updates

---
+++ Tic Tac Toe Game/064 First Python Milestone Project Overview.py
@@ -0,0 +1,12 @@
+game_list = [0, 1, 2,
+             3, 4, 5,
+             6, 7, 8]
+
+
+def display_game(game_list):
+    print("Here is the current list: ")
+    print(game_list)
+
+
+display_game(game_list)
+
